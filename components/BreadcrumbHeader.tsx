f773a68b7edda0e63d0c8ce73e6f7d18defc2124
minipingu <ranmaniax@gmail.com>
2025-03-13 17:09:17 +0700

    mobile navbar/sidebar

--- components/BreadcrumbHeader.tsx
+++ components/BreadcrumbHeader.tsx
@@ -7,12 +7,14 @@ import {
 	BreadcrumbLink,
 	BreadcrumbList,
 } from './ui/breadcrumb'
+import { MobileSidebar } from './Sidebar'
 
 const BreadcrumbHeader = () => {
 	const pathName = usePathname()
 	const path = pathName === '/' ? [''] : pathName?.split('/')
 	return (
 		<div className='flex items-center flex-start'>
+			<MobileSidebar />
 			<Breadcrumb>
 				<BreadcrumbList>
 					{path.map((path, index) => (
