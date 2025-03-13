0158324dffd04d25de390fecb8516496b1b20b21
minipingu <ranmaniax@gmail.com>
2025-03-13 15:21:55 +0700

    home (logo, sidebar, breadcrump, dark mode toggle)

---
+++ components/BreadcrumbHeader.tsx
@@ -0,0 +1,35 @@
+'use client'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+import {
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbList,
+} from './ui/breadcrumb'
+
+const BreadcrumbHeader = () => {
+	const pathName = usePathname()
+	const path = pathName === '/' ? [''] : pathName?.split('/')
+	return (
+		<div className='flex items-center flex-start'>
+			<Breadcrumb>
+				<BreadcrumbList>
+					{path.map((path, index) => (
+						<>
+							<BreadcrumbItem>
+								<BreadcrumbLink
+									className='capitalize'
+									href={`/${path}`}>
+									{path === '' ? 'home' : path}
+								</BreadcrumbLink>
+							</BreadcrumbItem>
+						</>
+					))}
+				</BreadcrumbList>
+			</Breadcrumb>
+		</div>
+	)
+}
+
+export default BreadcrumbHeader
